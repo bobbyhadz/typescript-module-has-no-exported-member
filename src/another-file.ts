c222a00297a08b7deb07e5eec4bc3ab3c38977b4
bobbyhadz <opmftw@gmail.com>
2024-02-28 16:05:41 +0200

    initial commit

---
+++ src/another-file.ts
@@ -0,0 +1,9 @@
+// 👇️ named export
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+const num = 350;
+
+// 👇️ default export
+export default num;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// 👇️ default and named imports
+import num, { sum } from './another-file';
+
+console.log(sum(num, num)); // 👉️ 700
